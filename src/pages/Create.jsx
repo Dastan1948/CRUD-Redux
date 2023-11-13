591bf11388e631dcbaa2611f57f70c64f5b561f8
Dastan <hakerdastan@gmail.com>
2023-11-13 21:55:17 +0300

    add react-hook-form

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,38 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux'
-import { addUser } from '../store/slices/userSlice'
 import { useNavigate } from 'react-router-dom'
+import { addUser } from '../store/slices/userSlice'
 
 const Create = () => {
-	const [user, setUser] = useState({
-    name: '',
-    email: ''
-  })
-
-  const { users } = useSelector(state => state.users)
+	const { users } = useSelector(state => state.users)
 	const dispatch = useDispatch()
 
 	const navigate = useNavigate()
 
-	const handleSubmit = e => {
-		e.preventDefault()
+	const {
+		register,
+		formState: { errors, isValid },
+		handleSubmit
+	} = useForm({
+		mode: 'onBlur',
+	})
 
-    let obj = {
-      id: users[users.length - 1].id + 1,
-      name: user.name,
-      email: user.email
-    }
+	const onSubmit = data => {
 
-		dispatch(addUser(obj))
+		data = {...data, id: users[users.length - 1].id + 1}
 
-    navigate('/')
+		dispatch(addUser(data))
+
+		navigate('/')
 	}
 
 	return (
 		<div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
 			<div className='w-50 border bg-secondary text-white p-5'>
 				<h3>Add New User</h3>
-				<form onSubmit={handleSubmit}>
+				<form onSubmit={handleSubmit(onSubmit)}>
 					<div>
 						<label htmlFor='name'>Name:</label>
 						<input
@@ -40,23 +39,39 @@ const Create = () => {
 							name='name'
 							className='form-control'
 							placeholder='enter name'
-              value={user.name}
-							onChange={e => setUser({...user, name: e.target.value})}
+							{...register('name', {
+								required: 'Поле обязательно нужно заполнить',
+								minLength: {
+									value: 2,
+									message: 'Нужно минимум 2 символа',
+								},
+							})}
 						/>
+						<div style={{ height: 20 }}>
+							{errors?.name && <p className='text-danger'>{errors?.name?.message || 'Error!'}</p>}
+						</div>
 					</div>
 					<div>
 						<label htmlFor='email'>Email:</label>
 						<input
-							type='text'
+							type='email'
 							name='email'
 							className='form-control'
 							placeholder='enter email'
-              value={user.email}
-							onChange={e => setUser({...user, email: e.target.value})}
+							{...register('email', {
+								required: 'Поле обязательно нужно заполнить',
+								minLength: {
+									value: 5,
+									message: 'Нужно минимум 5 символа',
+								},
+							})}
 						/>
+						<div style={{ height: 20 }}>
+							{errors?.email && <p className='text-danger'>{errors?.email?.message || 'Error!'}</p>}
+						</div>
 					</div>
 					<br />
-					<button className='btn btn-info'>Submit</button>
+					<button className='btn btn-info' disabled={!isValid}>Submit</button>
 				</form>
 			</div>
 		</div>
